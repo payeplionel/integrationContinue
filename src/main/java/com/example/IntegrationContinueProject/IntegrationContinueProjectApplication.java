11b370f7540f39046b9637d73e5c8e9e28e2801f
PAYEP lionel <payeplionel2@outlook.com>
2021-04-15 07:38:40 +0200

    first commit

---
+++ src/main/java/com/example/IntegrationContinueProject/IntegrationContinueProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.IntegrationContinueProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationContinueProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationContinueProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IntegrationContinueProject/IntegrationContinueProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntegrationContinueProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationContinueProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
